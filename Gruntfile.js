cecedc80bf8d355e83689819a429ac61809d675b
mlytle4218 <mlytle4218@gmail.com>
2018-10-01 12:56:21 -0700

    Update to make vscode happy

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function (grunt) {
+
+    //project configurations
+    grunt.initConfig({
+        uglify: {
+            options: {
+                banner: "/*! <%= grunt.template.today() %> */\n"
+            },
+            build: {
+                src: ["js/Initialize.js"],
+                dest: "js/threesixty_initialize.js"
+            }
+        },
+        watch: {
+            scripts: {
+                files: ["js/Initialize.js"],
+                tasks: ["uglify"]
+            }
+        }
+    });
+
+    //load uglify plugin
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    //create default task
+    grunt.registerTask("default", ["uglify"]);
+
+    //watch files
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+};
